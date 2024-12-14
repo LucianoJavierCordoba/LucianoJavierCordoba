ff931f529330d7fdbe5c2c0f04d7c0eedf47658e
1812 <francogadea1812@gmail.com>
2024-12-13 23:40:48 -0300

    Agregando videoJS y odometer para la animacion de los numeros asi como numberflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,18 @@
 import { defineConfig } from 'astro/config';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-
-    devToolbar: {
-        enabled: false
-    },
-    
-    i18n: {
-        defaultLocale: "es",
-        locales: ["es", "en",],
-        routing: {
-            prefixDefaultLocale: false
-        }
+  devToolbar: {
+    enabled: false
+  },
+  i18n: {
+    defaultLocale: "es",
+    locales: ["es", "en"],
+    routing: {
+      prefixDefaultLocale: false
     }
+  },
+  integrations: [react()]
 });
